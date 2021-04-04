8a49daf39c2d27274e9d5801f4898ceea5c1ae66
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-04-04 21:26:06 +0500

    Add Todo Task Implementation.

--- lib/models/Todo.dart
+++ lib/models/Todo.dart
@@ -2,5 +2,5 @@ class Todo {
   String text;
   bool done;
 
-  Todo({this.text, this.done});
+  Todo({this.text, this.done = false});
 }
--- lib/screens/TodoScreen.dart
+++ lib/screens/TodoScreen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_todo_app/controllers/TodoController.dart';
+import 'package:getx_todo_app/models/Todo.dart';
 
 class TodoScreen extends StatelessWidget {
+  final TodoController todoController = Get.find();
+  TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +18,7 @@ class TodoScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 30),
                 child: TextField(
                   autofocus: true,
+                  controller: textEditingController,
                   decoration: InputDecoration(
                       hintText: 'Enter What you want to accomplished.',
                       border: InputBorder.none,
@@ -31,12 +37,18 @@ class TodoScreen extends StatelessWidget {
                 RaisedButton(
                   child: Text('Cancel', style: TextStyle(color: Colors.white)),
                   color: Colors.red,
-                  onPressed: () {},
+                  onPressed: () {
+                    Get.back();
+                  },
                 ),
                 RaisedButton(
                   child: Text('Add', style: TextStyle(color: Colors.white)),
                   color: Colors.green,
-                  onPressed: () {},
+                  onPressed: () {
+                    todoController.todos
+                        .add(Todo(text: textEditingController.text));
+                    Get.back();
+                  },
                 ),
               ],
             )
